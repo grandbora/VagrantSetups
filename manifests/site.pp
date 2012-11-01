d9325977c9da3f06e3861a505a31431c4b7e7df3
Bora Tunca <bt@itembase.biz>
2012-11-01 20:18:50 +0100

    fu fu fu

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,11 +3,10 @@ Exec { path => "/usr/bin:/usr/sbin:/bin", }
 include bootstrap
 
 include apache
-include php
-include php::extensions::mysql
-include php::extensions::mcrypt
-include php::extensions::curl
-include php::extensions::phpunit
+include php::apache2
+# include php
+# include php::extensions::mysql
+# include php::extensions::mcrypt
+# include php::extensions::curl
+# include php::extensions::phpunit
 include mysql::server
-
-include kohana
